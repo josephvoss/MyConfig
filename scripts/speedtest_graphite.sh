cb07d21717bfb29cd1d5cda2df6366cb9341b9a5
Joseph Voss <josephvoss@utexas.edu>
2017-03-03 06:04:35 +0000

    Updating to use more accurate speedtest metric tespeed

--- scripts/speedtest_graphite.sh
+++ scripts/speedtest_graphite.sh
@@ -3,18 +3,25 @@
 CARBON_HOST="graphite.jvoss14.com"
 CARBON_PORT="2003"
 
-TEST_BIN="/home/zahir/speedtest-cli/speedtest.py"
+TEST_BIN="/home/zahir/tespeed/tespeed.py -sw -dt 5 -ut 5 -cs 51200 http://austx-speedtest-01-a.texas.rr.com/speedtest/"
 
 TEST_LOG=$($TEST_BIN)
 
-PING=$(echo $TEST_LOG | tr -d "\n" | sed "s/:\ /\n/g" | awk '{print $1}' | sed -n '2p')
-DOWNLOAD=$(echo $TEST_LOG | tr -d "\n" | sed "s/:\ /\n/g" | awk '{print $1}' | sed -n '3p')
-UPLOAD=$(echo $TEST_LOG | tr -d "\n" | sed "s/:\ /\n/g" | awk '{print $1}' | sed -n '4p')
+PING=$(ping -c 5 8.8.8.8 | grep avg)
+PING_MAX=$(echo $PING | awk -F/ '{print $6}')
+PING_MIN=$(echo $PING | awk -F/ '{print $4}')
+PING_AVG=$(echo $PING | awk -F/ '{print $5}')
+
+DOWNLOAD=$(echo $TEST_LOG | awk -F, '{print $1}')
+UPLOAD=$(echo $TEST_LOG | awk -F, '{print $2}')
 
 TIME=$(date +%s)
 
-echo "speedtest.ping" ${PING} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
+echo "speedtest.ping.avg" ${PING_AVG} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
+echo "speedtest.ping.min" ${PING_MIN} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
+echo "speedtest.ping.max" ${PING_MAX} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
 echo "speedtest.download" ${DOWNLOAD} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
 echo "speedtest.upload" ${UPLOAD} ${TIME} | nc ${CARBON_HOST} ${CARBON_PORT}
 
+echo "$PING_AVG $DOWNLOAD $UPLOAD"
 echo "Successful speedtest upload"
