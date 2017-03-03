4a01a4a42a10b3edd10f7948642ede09bfdd9e1a
Joseph Voss <josephvoss@utexas.edu>
2017-03-03 06:26:19 +0000

    Bug fixes syntatical errors

--- scripts/speedtest_graphite.sh
+++ scripts/speedtest_graphite.sh
@@ -7,10 +7,10 @@ TEST_BIN="/home/zahir/tespeed/tespeed.py -sw -dt 5 -ut 5 -cs 51200 http://austx-
 
 TEST_LOG=$($TEST_BIN)
 
-PING=$(ping -c 5 8.8.8.8 | grep avg)
-PING_MAX=$(echo $PING | awk -F/ '{print $6}')
-PING_MIN=$(echo $PING | awk -F/ '{print $4}')
-PING_AVG=$(echo $PING | awk -F/ '{print $5}')
+PING=$(ping -c 5 8.8.8.8 | grep avg | awk '{print $4}')
+PING_MAX=$(echo $PING | awk -F/ '{print $3}')
+PING_MIN=$(echo $PING | awk -F/ '{print $1}')
+PING_AVG=$(echo $PING | awk -F/ '{print $2}')
 
 DOWNLOAD=$(echo $TEST_LOG | awk -F, '{print $1}')
 UPLOAD=$(echo $TEST_LOG | awk -F, '{print $2}')
